ac00164114a619ead7b460266d2439453b54ba0b
Leon Barrett <leon@barrettnexus.com>
2021-03-01 21:40:05 -0800

    Clean up front matter and commentary

--- just-mode.el
+++ just-mode.el
@@ -8,42 +8,32 @@
 ;; Package-Requires: ((emacs "26.1"))
 ;; Keywords: files languages tools
 ;; URL: https://github.com/leon-barrett/just-mode.el
-;;
-;; Major mode for "just" https://github.com/casey/just
-;;
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
+
 ;; This file is *NOT* part of GNU Emacs
-;;
+
 ;; This package is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-;;
+
 ;; This package is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;;
+
 ;; You should have received a copy of the GNU General Public License
 ;; along with this package.  If not, see <http://www.gnu.org/licenses/>.
-;;
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
+
 ;;; Commentary:
-;;
-;; Description:
-;;
+
 ;; A major mode for editing justfiles, as defined by the tool "just":
 ;; https://github.com/casey/just
-;;
-;; This depends on Emacs 26.1 for `prog-first-column'.
-;;
-;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;
+
 ;;; Code:
 
 
+
+;; NOTE: This depends on Emacs 26.1 for `prog-first-column'.
 
 (require 'prog-mode)
 
